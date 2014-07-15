acaa8a6a8f015816dc6d411ee2c67b6aa53406ec
Andrew Reida <andrew@syn0.com>
2014-07-15 16:01:07 -0700

    Updating lively recipe

--- synapse/recipes/lively.rb
+++ synapse/recipes/lively.rb
@@ -6,7 +6,7 @@ git "lively" do
     repository "https://github.com/synapsestudios/lively"
     revision "master"
     user node['server']['user']
-    destination "/home/#{node['server']['user']}/lively"
+    destination "#{node['lively']['docroot']}"
     action :sync
 end
 
@@ -16,7 +16,7 @@ end
 
 execute "npm-install-lively" do
     creates "node_modules"
-    cwd "/home/#{node['server']['user']}/lively"
+    cwd "#{node['lively']['docroot']}"
     command "npm install"
     user node['server']['user']
     environment ({ "HOME" => "/home/#{node['server']['user']}" })
@@ -24,14 +24,46 @@ end
 
 execute "bower-install-lively" do
     creates "bower_components"
-    cwd "/home/#{node['server']['user']}/lively"
+    cwd "#{node['lively']['docroot']}"
     command "bower install"
     user node['server']['user']
     environment ({ "HOME" => "/home/#{node['server']['user']}" })
 end
 
+web_app node["lively"]["server_name"] do
+    server_name    node["lively"]["server_name"]
+    server_aliases node["lively"]["server_aliases"]
+    template       "lively.conf.erb"
+end
+
+execute "lively-copy-config" do
+    creates "docs"
+    cwd "/home/#{node['server']['user']}"
+    command "cp -a #{node['server']['docroot']}/docs/ #{node['lively']['confroot']}/"
+    user node['server']['user']
+    environment ({ "HOME" => "/home/#{node['server']['user']}" })
+end
+
+execute "lively-inject-hostname" do
+    creates "docs"
+    cwd "#{node['lively']['confroot']}"
+    command "sed -i 's/%HOSTNAME%/#{node['server']['server_name']}/g' config.*.js"
+    user node['server']['user']
+    environment ({ "HOME" => "/home/#{node['server']['user']}" })
+end
+
 # Default config for lively
-template "/home/#{node['server']['user']}/lively/application/config.js" do
+template "#{node['lively']['docroot']}/application/config.js" do
     source "lively-config.erb"
     mode 0655
+end
+
+supervisor_service "lively" do
+    user node['server']['user']
+    directory "#{node['lively']['docroot']}"
+    action [:enable, :start]
+    autostart true
+    autorestart true
+    command "gulp"
+    environment ({ "HOME" => "/home/#{node['server']['user']}" })
 end
