7a4d5e7f1acc1b4e2c8fa4698226cd34b0963e1f
Andrew Reida <andrew@syn0.com>
2014-08-08 15:56:52 -0700

    Updating lively recipe to work out of the box

--- synapse/recipes/lively.rb
+++ synapse/recipes/lively.rb
@@ -2,16 +2,15 @@ package "git" do
     action :install
 end
 
+npm_package "gulp"
+npm_package "bower"
+
 git "lively" do
     repository "https://github.com/synapsestudios/lively"
     revision "master"
     user node['server']['user']
     destination "#{node['lively']['docroot']}"
     action :sync
-end
-
-execute "npm-install-gulp" do
-    command "npm -g install gulp bower"
 end
 
 execute "npm-install-lively" do
@@ -39,7 +38,7 @@ end
 execute "lively-copy-config" do
     creates "docs"
     cwd "/home/#{node['server']['user']}"
-    command "cp -a #{node['server']['docroot']}/docs/ #{node['lively']['confroot']}/"
+    command "cp -a #{node['server']['docroot']}/docs/config/ #{node['lively']['confroot']}/"
     user node['server']['user']
     environment ({ "HOME" => "/home/#{node['server']['user']}" })
 end
