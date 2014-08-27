183fc3cdfb6e8dd07fc48b9136e9ad1369dc6181
Paul Statezny <paul@synapsestudios.com>
2014-08-27 10:55:08 -0700

    Refs #11 - Send correct command to mysql service.

--- synapse/recipes/dev_mysql_public.rb
+++ synapse/recipes/dev_mysql_public.rb
@@ -3,12 +3,5 @@ execute "make-mysql-server-public" do
     command "sed -i 's/bind-address/# bind-address/g' my.cnf"
     user 'root'
     environment ({ "HOME" => "/home/#{node['server']['user']}" })
-    notifies :reload, 'service[mysql]'
-end
-
-execute "restart-mysql-server" do
-    cwd "/etc/mysql"
-    command "service mysql restart"
-    user 'root'
-    environment ({ "HOME" => "/home/#{node['server']['user']}" })
+    notifies :restart, 'service[mysql]'
 end
