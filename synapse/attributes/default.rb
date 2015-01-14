32b6592e815812a4cd50e2a57dfbf8e4feca29dc
Zachary Chavez <zach@synapsestudios.com>
2015-01-14 15:05:48 -0700

    Refs #13 - Update default charset and collation values in config file template.

--- synapse/attributes/default.rb
+++ synapse/attributes/default.rb
@@ -1,3 +1,3 @@
-node.default.mysql_charset.encoding  = 'utf8'
-node.default.mysql_charset.collation = 'utf8_unicode_ci'
+node.default.mysql_charset.encoding  = 'utf8mb4'
+node.default.mysql_charset.collation = 'utf8mb4_unicode_ci'
 node.default.mysql.confd_dir         = "/etc/mysql/conf.d"
